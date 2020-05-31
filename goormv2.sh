b203a71d2b209922785e3be5ce3c9a7cebf0b066
小朋 <2810580330@qq.com>
2020-06-01 07:56:12 +0800

    Update goormv2.sh

--- goormv2.sh
+++ goormv2.sh
@@ -5,14 +5,11 @@ apt-get install -y screen
 rm -rf /v2ray
 mkdir /v2ray
 cd /v2ray
-rm -f /v2ray/v2ray
 wget --no-check-certificate https://github.com/byxiaopeng/goorm-v2ray/raw/master/v2ray
 #设置运行权限
 chmod +x /v2ray/v2ray
-rm -f /v2ray/v2ctl
 wget --no-check-certificate https://github.com/byxiaopeng/goorm-v2ray/raw/master/v2ctl
 chmod +x /v2ray/v2ctl
-rm -f /v2ray/config.json
 wget --no-check-certificate https://github.com/byxiaopeng/goorm-v2ray/raw/master/config.json
 
 
