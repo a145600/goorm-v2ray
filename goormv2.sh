34b5b151ed87bcedb983c57884a2852a268a5d3c
小朋 <2810580330@qq.com>
2020-06-07 16:04:14 +0800

    Update goormv2.sh

--- goormv2.sh
+++ goormv2.sh
@@ -4,7 +4,7 @@ apt-get -y update
 apt install curl -y
 apt install unzip -y
 mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/byxiaopeng/goorm-v2ray/raw/master/v2ray-linux-64.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
