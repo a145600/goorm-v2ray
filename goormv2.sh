3993559cba7e579efb45cf6d89c62ae528807a7c
splenddd <56500306+splenddd@users.noreply.github.com>
2020-06-23 18:51:44 +0800

    Update goormv2.sh

--- goormv2.sh
+++ goormv2.sh
@@ -11,7 +11,3 @@ wget --no-check-certificate https://github.com/splenddd/goorm-v2ray/raw/master/v
 chmod +x /v2ray/v2ctl
 wget --no-check-certificate https://github.com/splenddd/goorm-v2ray/raw/master/config.json
 
-#修改系统为北京时间
-cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-# 开始后台运行
-nohup /v2ray/v2ray -config=/v2ray/config.json >out.txt 2>&1 &
